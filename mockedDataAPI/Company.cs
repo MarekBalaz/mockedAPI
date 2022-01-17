88dd454ce03f1d44a6825edcb5e86fa7bf19fbde
Marek Balaz <marekgamingacc@gmail.com>
2022-01-17 21:19:51 +0100

    created and finnished mocked data api

---
+++ mockedDataAPI/Company.cs
@@ -0,0 +1,77 @@
+﻿namespace MainFinanceAPI.Model_Classes
+{
+    public class Company
+    {
+        public string symbol { get; set; }
+        public float dividendYielTTM { get; set; }
+        public float dividendYielPercentageTTM { get; set; }
+        public float peRatioTTM { get; set; }
+        public float pegRatioTTM { get; set; }
+        public float payoutRatioTTM { get; set; }
+        public float currentRatioTTM { get; set; }
+        public float quickRatioTTM { get; set; }
+        public float cashRatioTTM { get; set; }
+        public float daysOfSalesOutstandingTTM { get; set; }
+        public float daysOfInventoryOutstandingTTM { get; set; }
+        public float operatingCycleTTM { get; set; }
+        public float daysOfPayablesOutstandingTTM { get; set; }
+        public float cashConversionCycleTTM { get; set; }
+        public float grossProfitMarginTTM { get; set; }
+        public float operatingProfitMarginTTM { get; set; }
+        public float pretaxProfitMarginTTM { get; set; }
+        public float netProfitMarginTTM { get; set; }
+        public float effectiveTaxRateTTM { get; set; }
+        public float returnOnAssetsTTM { get; set; }
+        public float returnOnEquityTTM { get; set; }
+        public float returnOnCapitalEmployedTTM { get; set; }
+        public float netIncomePerEBTTTM { get; set; }
+        public float ebtPerEbitTTM { get; set; }
+        public float ebitPerRevenueTTM { get; set; }
+        public float debtRatioTTM { get; set; }
+        public float debtEquityRatioTTM { get; set; }
+        public float longTermDebtToCapitalizationTTM { get; set; }
+        public float totalDebtToCapitalizationTTM { get; set; }
+        public float interestCoverageTTM { get; set; }
+        public float cashFlowToDebtRatioTTM { get; set; }
+        public float companyEquityMultiplierTTM { get; set; }
+        public float receivablesTurnoverTTM { get; set; }
+        public float payablesTurnoverTTM { get; set; }
+        public float inventoryTurnoverTTM { get; set; }
+        public float fixedAssetTurnoverTTM { get; set; }
+        public float assetTurnoverTTM { get; set; }
+        public float operatingCashFlowPerShareTTM { get; set; }
+        public float freeCashFlowPerShareTTM { get; set; }
+        public float cashPerShareTTM { get; set; }
+        public float operatingCashFlowSalesRatioTTM { get; set; }
+        public float freeCashFlowOperatingCashFlowRatioTTM { get; set; }
+        public float cashFlowCoverageRatiosTTM { get; set; }
+        public float shortTermCoverageRatiosTTM { get; set; }
+        public float capitalExpenditureCoverageRatioTTM { get; set; }
+        public float dividendPaidAndCapexCoverageRatioTTM { get; set; }
+        public float priceBookValueRatioTTM { get; set; }
+        public float priceToBookRatioTTM { get; set; }
+        public float priceToSalesRatioTTM { get; set; }
+        public float priceEarningsRatioTTM { get; set; }
+        public float priceToFreeCashFlowsRatioTTM { get; set; }
+        public float priceToOperatingCashFlowsRatioTTM { get; set; }
+        public float priceCashFlowRatioTTM { get; set; }
+        public float priceEarningsToGrowthRatioTTM { get; set; }
+        public float priceSalesRatioTTM { get; set; }
+        public float dividendYieldTTM { get; set; }
+        public float enterpriseValueMultipleTTM { get; set; }
+        public float priceFairValueTTM { get; set; }
+        public float dividendPerShareTTM { get; set; }
+    }
+    
+        
+    public class DCF
+    {
+        public string symbol { get; set; }
+        public string date { get; set; }
+        public float dcf { get; set; }
+        public float StockPrice { get; set; }
+    }
+
+
+
+}
---
+++ mockedDataAPI/Controllers/MockedData.cs
@@ -0,0 +1,67 @@
+﻿using MainFinanceAPI.Model_Classes;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace mockedDataAPI.Controllers
+{
+    public class MockedData : ControllerBase
+    {
+        public ILogger<MockedData> logger { get; set; }
+        public MockedData(ILogger<MockedData> _logger)
+        {
+            logger = _logger;
+        }
+        [HttpGet]
+        [Route("mockeddata/listofcompanies")]
+        public string ListOfCompanies()
+        {
+            StreamReader sr = new StreamReader("MockedDataFolder/MockDataList.txt");
+            string jsonData = sr.ReadToEnd();
+            sr.Close();
+            return jsonData;
+        }
+        [HttpGet]
+        [Route("mockeddata/listofratios")]
+        public string RatiosData([Optional] string ticker)
+        {
+            string response = "";
+            StreamReader sr = new StreamReader("MockedDataFolder/MockData.txt");
+            List<Company> companies = JsonSerializer.Deserialize<List<Company>>(sr.ReadToEnd());
+            sr.Close();
+            if(ticker == null)
+            {
+                response = JsonSerializer.Serialize(companies);
+                return response;
+            }
+            else
+            {
+                Company company = companies.Where(x => x.symbol == ticker).First();
+                response = JsonSerializer.Serialize(company);
+                return response;
+            }
+            
+        }
+        [HttpGet]
+        [Route("mockeddata/dcf")]
+        public string DCFData([Optional] string ticker)
+        {
+            string response = "";
+            StreamReader sr = new StreamReader("MockedDataFolder/MockDCF.txt");
+            List<DCF> dcfList = JsonSerializer.Deserialize<List<DCF>>(sr.ReadToEnd());
+            if(ticker == null)
+            {
+                response = JsonSerializer.Serialize(dcfList);
+                return response;
+            }
+            else
+            {
+                DCF dcf = dcfList.Where(x => x.symbol == ticker).First();
+                response = JsonSerializer.Serialize(dcf);
+                return response;
+            }
+            return "";
+        }
+    }
+}
